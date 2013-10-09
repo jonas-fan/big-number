15dc438ac0d62a631fde90a01775360c92a64c64
Jian <john33550@gmail.com>
2013-10-09 10:18:47 +0800

    example

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    Large::Number x = -25;
-    Large::Number y = -10;
+    Large::Number x("78962132878965235699846513216889615615631868464612316613153153153521898");
+    Large::Number y("813048956");
 
     std::cout << "x = " << x << "\ny = " << y << "\n" << std::endl;
 
